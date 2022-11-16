8742047f3836ecd17341ab70962f655bdca359a5
Stanovov Mikhail <fuser-mgn@ya.ru>
2022-11-16 16:38:32 +0500

    123

--- main.js
+++ main.js
@@ -2,14 +2,12 @@ const UI_ELEMENTS = {
     SETTING_BUTTON: document.querySelector('.setting-button'),
     SETTINGS_MODAL_CLOSE: document.querySelector('.settings-modal__close'),
     SETTINGS_MODAL: document.querySelector('.settings-modal'),
-}
+};
 
 UI_ELEMENTS.SETTING_BUTTON.addEventListener('click', function() {
-    const modal = UI_ELEMENTS.SETTINGS_MODAL;
-    modal.classList.remove('hide');
+    UI_ELEMENTS.SETTINGS_MODAL.classList.remove('hide');
 });
 
 UI_ELEMENTS.SETTINGS_MODAL_CLOSE.addEventListener('click', function() {
-    const modal = UI_ELEMENTS.SETTINGS_MODAL;
-    modal.classList.add('hide');
-})
+    UI_ELEMENTS.SETTINGS_MODAL.classList.add('hide');
+});
