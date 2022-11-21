5d6438530a23a2012c45755e0f9d8de5a0511c72
Stanovov Mikhail <98664376+CTEPXMGN@users.noreply.github.com>
2022-11-21 16:52:45 +0500

    123

--- main.js
+++ main.js
@@ -12,9 +12,21 @@ const UI_ELEMENTS = {
     COMPANION_TMPL: document.querySelector('#companion-tmpl').content,
     CHAT_FIELD: document.querySelector('.chat-field'),
     SEND_FORM: document.querySelector('.send-form'),
+    SIGN_BUTTON: document.querySelector('.sign-button'),
+    SIGN_MODAL_BUTTON: document.querySelector('.sign-modal__button'),
+    SIGN_MODAL_FORM: document.querySelector('.sign-modal__form'),
+    SIGN_MODAL: document.querySelector('.sign-modal'),
+    SIGN_MODAL_CLOSE: document.querySelector('.sign-modal__close'),
+    SIGN_MODAL_INPUT_EMAIL: document.querySelector('.sign-modal__input-email'),
 };
 
-let myName = '';
+let user = {
+    name: '',
+    isSigned: false,
+};
+
+let email = {};
+const URL = 'https://edu.strada.one/api/user';
 
 UI_ELEMENTS.SETTINGS_BUTTON.addEventListener('click', showSettings);
 UI_ELEMENTS.SETTINGS_MODAL_CLOSE.addEventListener('click', hideSettings);
@@ -22,10 +34,14 @@ UI_ELEMENTS.SEND_BUTTON.addEventListener('click', sendMessage);
 UI_ELEMENTS.SEND_FORM.addEventListener('submit', sendMessage);
 UI_ELEMENTS.SETTINGS_MODAL_FORM.addEventListener('submit', saveName);
 UI_ELEMENTS.SETTINGS_MODAL_BUTTON.addEventListener('click', saveName);
+UI_ELEMENTS.SIGN_BUTTON.addEventListener('click', showSignIn);
+UI_ELEMENTS.SIGN_MODAL_BUTTON.addEventListener('click', getCode);
+UI_ELEMENTS.SIGN_MODAL_FORM.addEventListener('submit', getCode);
+UI_ELEMENTS.SIGN_MODAL_CLOSE.addEventListener('click', hideSignIn);
 
 function saveName(event) {
     event.preventDefault();
-    myName = UI_ELEMENTS.SETTINGS_MODAL_INPUT_NAME.value;
+    user.isSigned = UI_ELEMENTS.SETTINGS_MODAL_INPUT_NAME.value;
     hideSettings();
     UI_ELEMENTS.USER_NAME.textContent = UI_ELEMENTS.SETTINGS_MODAL_INPUT_NAME.value;
     UI_ELEMENTS.SETTINGS_MODAL_INPUT_NAME.value = '';
@@ -44,13 +60,39 @@ function getTime() {
     return `${date.getHours()}:${date.getMinutes()}`;
 };
 
+function showSignIn() {
+    UI_ELEMENTS.SIGN_MODAL.classList.remove('hide');
+};
+
+function hideSignIn() {
+    UI_ELEMENTS.SIGN_MODAL.classList.add('hide');
+};
+
+async function getCode(event) {
+    event.preventDefault();
+
+    const myEmail = UI_ELEMENTS.SIGN_MODAL_INPUT_EMAIL.value;
+
+    let response = await fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(myEmail)
+    });
+
+    let result = await response.json();
+
+    console.log(result);
+};
+
 function sendMessage(event) {
     event.preventDefault();
     if (UI_ELEMENTS.INPUT_MESSAGE.value) {
         const myTmplClone = UI_ELEMENTS.MY_TMPL.cloneNode(true).querySelector('.my-message');
         const myTmplText = myTmplClone.querySelector('.message-text');
         const myTmplTime = myTmplClone.querySelector('.message-time');
-        myTmplText.textContent = myName + ': ' + UI_ELEMENTS.INPUT_MESSAGE.value;
+        myTmplText.textContent = user.isSigned + ': ' + UI_ELEMENTS.INPUT_MESSAGE.value;
         myTmplTime.textContent = getTime();
         UI_ELEMENTS.CHAT_FIELD.prepend(myTmplClone);
     
