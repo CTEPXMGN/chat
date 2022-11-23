ac13506da4bebf3dc80bd7a5ae607f5b52baffa1
Stanovov Mikhail <98664376+CTEPXMGN@users.noreply.github.com>
2022-11-23 22:48:27 +0500

    234

--- main.js
+++ main.js
@@ -1,46 +1,10 @@
-const UI_ELEMENTS = {
-    SETTINGS_BUTTON: document.querySelector('.setting-button'),
-    SETTINGS_MODAL_CLOSE: document.querySelector('.settings-modal__close'),
-    SETTINGS_MODAL: document.querySelector('.settings-modal'),
-    SETTINGS_MODAL_FORM: document.querySelector('.settings-modal__form'),
-    SETTINGS_MODAL_INPUT_NAME: document.querySelector('.settings-modal__input-name'),
-    SETTINGS_MODAL_BUTTON: document.querySelector('.settings-modal__button'),
-    USER_NAME: document.querySelector('.user-name'),
-    SEND_BUTTON: document.querySelector('.send-button'),
-    INPUT_MESSAGE: document.querySelector('.message-input'),
-    MY_TMPL: document.querySelector('#my-tmpl').content,
-    COMPANION_TMPL: document.querySelector('#companion-tmpl').content,
-    CHAT_FIELD: document.querySelector('.chat-field'),
-    SEND_FORM: document.querySelector('.send-form'),
-    SIGN_BUTTON: document.querySelector('.sign-button'),
-    SIGN_MODAL_BUTTON: document.querySelector('.sign-modal__button'),
-    SIGN_MODAL_FORM: document.querySelector('.sign-modal__form'),
-    SIGN_MODAL: document.querySelector('.sign-modal'),
-    SIGN_MODAL_CLOSE: document.querySelector('.sign-modal__close'),
-    SIGN_MODAL_INPUT_EMAIL: document.querySelector('.sign-modal__input-email'),
-    CONFIRM_MODAL: document.querySelector('.confirm-modal'),
-    CONFIRM_MODAL_CLOSE: document.querySelector('.confirm-modal__close'),
-    CONFIRM_MODAL_FORM: document.querySelector('.confirm-modal__form'),
-    CONFIRM_MODAL_INPUT_TOKEN: document.querySelector('.confirm-modal__input-token'),
-    CONFIRM_MODAL_BUTTON: document.querySelector('.confirm-modal__button'),
-};
-
-let user = {
-    name: '',
-    isSigned: false,
-};
-
-let email = {};
-const URL = 'https://edu.strada.one/api/user';
+import {UI_ELEMENTS, URLS} from "./view.js";
 
 UI_ELEMENTS.SETTINGS_BUTTON.addEventListener('click', showSettings);
 UI_ELEMENTS.SETTINGS_MODAL_CLOSE.addEventListener('click', hideSettings);
-UI_ELEMENTS.SEND_BUTTON.addEventListener('click', sendMessage);
 UI_ELEMENTS.SEND_FORM.addEventListener('submit', sendMessage);
-UI_ELEMENTS.SETTINGS_MODAL_FORM.addEventListener('submit', saveName);
-UI_ELEMENTS.SETTINGS_MODAL_BUTTON.addEventListener('click', saveName);
+UI_ELEMENTS.SETTINGS_MODAL_FORM.addEventListener('submit', setName);
 UI_ELEMENTS.SIGN_BUTTON.addEventListener('click', showSignIn);
-UI_ELEMENTS.SIGN_MODAL_BUTTON.addEventListener('click', getCode);
 UI_ELEMENTS.SIGN_MODAL_FORM.addEventListener('submit', getCode);
 UI_ELEMENTS.SIGN_MODAL_CLOSE.addEventListener('click', hideSignIn);
 UI_ELEMENTS.CONFIRM_MODAL_CLOSE.addEventListener('click', hideConfirm);
@@ -87,43 +51,63 @@ function saveCoockie(event) {
     showSettings();
 };
 
-function saveName(event) {
-    event.preventDefault();
-    user.isSigned = UI_ELEMENTS.SETTINGS_MODAL_INPUT_NAME.value;
-    hideSettings();
-    UI_ELEMENTS.USER_NAME.textContent = UI_ELEMENTS.SETTINGS_MODAL_INPUT_NAME.value;
-    UI_ELEMENTS.SETTINGS_MODAL_INPUT_NAME.value = '';
+async function getHistory() {
+    const response = await fetch(URLS.URL_MESSAGES, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            'Authorization': `Bearer ${getCookie('token')}`,
+        },
+    });
+    let result = await response.json();
+    // console.log(result.messages);
+    const messages = result.messages;
+
+    for (let i = 0; i <= 10; i++) {
+        renderMessages(messages[i]);
+    };
 };
 
-async function setName() {
+getHistory();
+
+async function getUser() {
+
+    const response = await fetch(URLS.URL_NAME, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            'Authorization': `Bearer ${getCookie('token')}`,
+        },
+    });
+    let result = await response.json();
+    console.log(result);
+};
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
+
+async function setName(event) {
+    event.preventDefault();
     const myName = {
         name: UI_ELEMENTS.SETTINGS_MODAL_INPUT_NAME.value,
     }
 
-    const response = await fetch(URL, {
+    const response = await fetch(URLS.URL, {
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json;charset=utf-8',
-            'Authorization': ` ${token}`
+            'Authorization': `Bearer ${getCookie('token')}`,
         },
         body: JSON.stringify(myName),
     });
 
+    getUser();
+    hideSettings();
 };
-
-async function getUser() {
-
-    const response = await fetch(URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8',
-            'Authorization': ` ${token}`
-        },
-        body: JSON.stringify(myEmail),
-    });
-
-    let result = await response.json();
-}
 
 async function getCode(event) {
     event.preventDefault();
@@ -141,23 +125,36 @@ async function getCode(event) {
     });
 
     let result = await response.json();
+    console.log(result);
 
     hideSignIn();
     showConfirm();
 };
 
-function sendMessage(event) {
-    event.preventDefault();
-    if (UI_ELEMENTS.INPUT_MESSAGE.value) {
+function sendMessage(event, item) {
+    // event.preventDefault();
+    // if (UI_ELEMENTS.INPUT_MESSAGE.value) {
+        console.log(item);
         const myTmplClone = UI_ELEMENTS.MY_TMPL.cloneNode(true).querySelector('.my-message');
         const myTmplText = myTmplClone.querySelector('.message-text');
         const myTmplTime = myTmplClone.querySelector('.message-time');
-        myTmplText.textContent = user.isSigned + ': ' + UI_ELEMENTS.INPUT_MESSAGE.value;
+        // myTmplText.textContent = item.text + ': ' + UI_ELEMENTS.INPUT_MESSAGE.value;
+        myTmplText.textContent = item.user.name + ': ' + item.text;
         myTmplTime.textContent = getTime();
         UI_ELEMENTS.CHAT_FIELD.prepend(myTmplClone);
     
         UI_ELEMENTS.INPUT_MESSAGE.value = '';
-    } else {
-        alert('Введите сообщение.');
-    };
+    // } else {
+    //     alert('Введите сообщение.');
+    // };
 };
+
+function renderMessages(item) {
+    const companionTmplClone = UI_ELEMENTS.COMPANION_TMPL.cloneNode(true).querySelector('.companion-message');
+    const companionTmplText = companionTmplClone.querySelector('.message-text');
+    const companionTmplTime = companionTmplClone.querySelector('.message-time');
+    // myTmplText.textContent = item.text + ': ' + UI_ELEMENTS.INPUT_MESSAGE.value;
+    companionTmplText.textContent = item.user.name + ': ' + item.text;
+    companionTmplTime.textContent = getTime();
+    UI_ELEMENTS.CHAT_FIELD.prepend(companionTmplClone);
+}
