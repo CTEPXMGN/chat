fea98a87c5bb5449317c8cdd13fc96ddeb164dde
Stanovov Mikhail <fuser-mgn@ya.ru>
2022-11-24 11:54:28 +0500

    123

--- view.js
+++ view.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const UI_ELEMENTS = {
     SETTINGS_BUTTON: document.querySelector('.setting-button'),
     SETTINGS_MODAL_CLOSE: document.querySelector('.settings-modal__close'),
@@ -30,37 +29,3 @@ export const URLS ={
     URL: 'https://edu.strada.one/api/user',
     URL_NAME: 'https://edu.strada.one/api/user/me',
     URL_MESSAGES: 'https://edu.strada.one/api/messages',
-=======
-export const UI_ELEMENTS = {
-    SETTINGS_BUTTON: document.querySelector('.setting-button'),
-    SETTINGS_MODAL_CLOSE: document.querySelector('.settings-modal__close'),
-    SETTINGS_MODAL: document.querySelector('.settings-modal'),
-    SETTINGS_MODAL_FORM: document.querySelector('.settings-modal__form'),
-    SETTINGS_MODAL_INPUT_NAME: document.querySelector('.settings-modal__input-name'),
-    SETTINGS_MODAL_BUTTON: document.querySelector('.settings-modal__button'),
-    USER_NAME: document.querySelector('.user-name'),
-    SEND_BUTTON: document.querySelector('.send-button'),
-    INPUT_MESSAGE: document.querySelector('.message-input'),
-    MY_TMPL: document.querySelector('#my-tmpl').content,
-    COMPANION_TMPL: document.querySelector('#companion-tmpl').content,
-    CHAT_FIELD: document.querySelector('.chat-field'),
-    SEND_FORM: document.querySelector('.send-form'),
-    SIGN_BUTTON: document.querySelector('.sign-button'),
-    SIGN_MODAL_BUTTON: document.querySelector('.sign-modal__button'),
-    SIGN_MODAL_FORM: document.querySelector('.sign-modal__form'),
-    SIGN_MODAL: document.querySelector('.sign-modal'),
-    SIGN_MODAL_CLOSE: document.querySelector('.sign-modal__close'),
-    SIGN_MODAL_INPUT_EMAIL: document.querySelector('.sign-modal__input-email'),
-    CONFIRM_MODAL: document.querySelector('.confirm-modal'),
-    CONFIRM_MODAL_CLOSE: document.querySelector('.confirm-modal__close'),
-    CONFIRM_MODAL_FORM: document.querySelector('.confirm-modal__form'),
-    CONFIRM_MODAL_INPUT_TOKEN: document.querySelector('.confirm-modal__input-token'),
-    CONFIRM_MODAL_BUTTON: document.querySelector('.confirm-modal__button'),
-};
-
-export const URLS ={
-    URL: 'https://edu.strada.one/api/user',
-    URL_NAME: 'https://edu.strada.one/api/user/me',
-    URL_MESSAGES: 'https://edu.strada.one/api/messages',
->>>>>>> ac13506da4bebf3dc80bd7a5ae607f5b52baffa1
-};
